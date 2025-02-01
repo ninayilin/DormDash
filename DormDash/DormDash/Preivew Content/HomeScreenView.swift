26a2ef1dd9ed610adc3ed58b81a2c6b358f312e7
Nina Yang <165723571+ninayilin@users.noreply.github.com>
2025-02-01 14:34:36 -0500

    homescreen
    screen

--- DormDash/DormDash/Preivew Content/HomeScreenView.swift
+++ DormDash/DormDash/Preivew Content/HomeScreenView.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct HomeScreenView: View {
     var body: some View {
         /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+        
+        
+        Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+        }
     }
 }
 
